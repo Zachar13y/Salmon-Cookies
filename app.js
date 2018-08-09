0bc8e0557bb5a9c003b0d9abc5710e63f2282036
Zachar13y <zachar13y@gmail.com>
2018-08-08 17:13:36 -0700

    All stores put into objects, console logs created for each.

--- app.js
+++ app.js
@@ -6,13 +6,46 @@ function getRandomInt(min, max) {
 console.log(getRandomInt(20, 100));
 console.log(getRandomInt(4, 15));
 
-var store = {
+var pioneer = {
     location: 'Pioneer Square',
     minCustomers: 17,
     maxCustomers: 88,
     avgSale: 5.2,
     calculateAvg: function() {
         getRandomInt(this.minCustomers, this.maxCustomers);
-    },
+        return this.calculateAvg;
+    }
 }
-console.log(store);
+console.log(pioneer);
+
+var airport = {
+    location: 'Portland Airport',
+    minCustomers: 6,
+    maxCustomers: 24,
+    avgSale: 1.2
+}
+console.log(airport);
+
+var washingtonSquare = {
+    location: 'Washington Square',
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgSale: 1.9
+}
+console.log(washingtonSquare);
+
+var sellwood = {
+    location: 'Sellwood',
+    minCustomers: 20,
+    maxCustomers: 48,
+    avgSale: 3.3
+}
+console.log(sellwood);
+
+var pearlDistrict = {
+    location: 'Pearl District',
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgSale: 2.6
+}
+console.log(pearlDistrict);
