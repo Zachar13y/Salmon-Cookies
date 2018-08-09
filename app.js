0d0412ea2e41ad0c0098db78efc4450c5bf24e88
Zachar13y <zachar13y@gmail.com>
2018-08-08 18:50:10 -0700

    Added randomize methods to store objects, added background color to page.

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
 function getRandomInt(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    return Math.floor(Math.random() * (max - min)) + min;
+    console.log('Get Random int min: ' + min);
+    var mathMin = Math.ceil(min)
+    var mathMax = Math.floor(max)
+    return Math.floor(Math.random() * (mathMax - mathMin)) + mathMin;
 }
-console.log(getRandomInt(20, 100));
-console.log(getRandomInt(4, 15));
 
 var pioneer = {
     location: 'Pioneer Square',
     minCustomers: 17,
     maxCustomers: 88,
     avgSale: 5.2,
-    calculateAvg: function() {
-        getRandomInt(this.minCustomers, this.maxCustomers);
-        return this.calculateAvg;
+    randomizeCustomers: function() {
+       var randomCustomer = getRandomInt(this.minCustomers, this.maxCustomers);
+        return randomCustomer;
     }
 }
+var pioneerRandoms = pioneer.randomizeCustomers();
+console.log('Pioneer random customers: ' + pioneerRandoms);
 console.log(pioneer);
 
 var airport = {
     location: 'Portland Airport',
     minCustomers: 6,
     maxCustomers: 24,
-    avgSale: 1.2
+    avgSale: 1.2,
+    randomizeCustomers: function() {
+        var randomCustomer = getRandomInt(this.minCustomers, this.maxCustomers);
+        return randomCustomer;
+    }
 }
+var airportRandoms = airport.randomizeCustomers();
+console.log('Airport random customers' + airportRandoms);
 console.log(airport);
 
 var washingtonSquare = {
     location: 'Washington Square',
     minCustomers: 11,
     maxCustomers: 38,
-    avgSale: 1.9
+    avgSale: 1.9,
+    randomizeCustomers: function() {
+        var randomCustomer = getRandomInt(this.minCustomers, this.maxCustomers);
+        return randomCustomer;
+    }
 }
 console.log(washingtonSquare);
 
@@ -38,7 +49,11 @@ var sellwood = {
     location: 'Sellwood',
     minCustomers: 20,
     maxCustomers: 48,
-    avgSale: 3.3
+    avgSale: 3.3,
+    randomizeCustomers: function() {
+        var randomCustomer = getRandomInt(this.minCustomers, this.maxCustomers);
+        return randomCustomer;
+    }
 }
 console.log(sellwood);
 
@@ -46,6 +61,10 @@ var pearlDistrict = {
     location: 'Pearl District',
     minCustomers: 3,
     maxCustomers: 24,
-    avgSale: 2.6
+    avgSale: 2.6,
+    randomizeCustomers: function() {
+        var randomCustomer = getRandomInt(this.minCustomers, this.maxCustomers);
+        return randomCustomer;
+    }
 }
 console.log(pearlDistrict);
