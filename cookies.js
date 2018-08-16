26946804f9ae018209372e2e065e2888f8de795c
Zachar13y <zachar13y@gmail.com>
2018-08-15 19:59:35 -0700

    Made store constructor that appends cells, rows and data.

--- cookies.js
+++ cookies.js
@@ -3,3 +3,18 @@ var Store = function(location, minCustomers, maxCustomers) {
     this.Min.Customers = minCustomers;
     this.Max.Customers = maxCustomers;
 }
+var locationName = this.location.length;
+this.addToTable = function(table) {
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    cell.innerText = this.location;
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    cell.innerText = this.minCustomers;
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    cell.innerText = this.maxCustomers;
+    table.appendChild(row);
+}
+
+var s
