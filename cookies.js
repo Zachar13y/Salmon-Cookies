9287a3958c089a87461791d44265bc42f54c82ff
Zachar13y <zachar13y@gmail.com>
2018-08-16 20:42:21 -0700

    Created table template in table.html, two out of eight hours are printing.

--- cookies.js
+++ cookies.js
@@ -4,10 +4,10 @@ var Store = function(location, minCustomers, maxCustomers, avgCookies) {
     this.maxCustomers = maxCustomers;
     this.avgCookies = avgCookies;
 
-    ////////////Takes user input to add to the table
+    ////////////////////////////
     // var locationName = this.location.length;
     // var min = this.minCustomers.length;
-//////////////////////////////
+
     this.addToTable = function(table) {
         var row = document.createElement('tr');
         var cell = document.createElement('td');
@@ -28,13 +28,42 @@ var Store = function(location, minCustomers, maxCustomers, avgCookies) {
 }
 
 var stores = [];
-stores.push(new Store('Pioneer Square', 'Min. Customers: 17', 'Max. Customers: 88'));
-stores.push(new Store('Portland Airport', 'Min. Customers: 6', 'Max. Customers: 24'));
-stores.push(new Store('Washington Square', 'Min. Customers: 11', 'Max. Customers: 38'));
-stores.push(new Store('Sellwood', 'Min. Customers: 20', 'Max. Customers: 48'));
-stores.push(new Store('Pearl District', 'Min. Customers: 3', 'Max. Customers: 24'));
+stores.push(new Store('Pioneer Square', '17', '88'));
+stores.push(new Store('Portland Airport', '6', '24'));
+stores.push(new Store('Washington Square', '11', '38'));
+stores.push(new Store('Sellwood', '20', '48'));
+stores.push(new Store('Pearl District', '3', '24'));
 
 var referenceTable = document.getElementById('salmon-table').firstElementChild;
 for (var i=0; i < stores.length; i++) {
     stores[i].addToTable(referenceTable);
 }
+
+var Time = function(hour, cookies) {
+    this.hour = hour;
+    this.cookies = cookies;
+
+    this.addToTable = function(table) {
+    var row = document.createElement('tr');
+    var cell = document.createElement('tr');
+    cell.innerText = this.hour;
+    row.appendChild(cell);
+    cell = document.createElement('tr');
+    cell.innerText = this.cookies;
+    row.appendChild(cell);
+    cell = document.createElement('td');
+    table.appendChild(row);
+    }
+}
+
+
+var hours = [];
+hours.push(new Time('10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'));
+
+var referenceTime = document.getElementById('time-table').firstElementChild;
+for (var i=0; i < hours.length; i++) {
+    hours[i].addToTable(referenceTime);
+}
+
+
+
